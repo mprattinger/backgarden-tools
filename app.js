456b1354c59e5754ad0ff244dad9787fb94029de
Michael Prattinger <mprattinger@outlook.com>
2017-05-27 09:46:56 +0200

    app is now debugable

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var readline = require("readline");
+
 var WerferClass = require("./Gpios/werfer");
 var TropferClass = require("./Gpios/tropfer");
 var SprinklerClass = require("./Gpios/sprinkler");
@@ -14,88 +16,104 @@ var opt = require("node-getopt").create([
     ["s", "sprinkler", "Sprinkler"],
     ["t", "tropfer", "Tropfer"],
     ["l", "lamp", "Lampe einschalten"],
-    ["p", "power", "Trafo einschalten"]
+    ["p", "power", "Trafo einschalten"],
+    ["d", "debug", "Debuggen"]
 ]).bindHelp().parseSystem();
 
 var config = new configM();
 config.runConfiguration();
 
-if(opt.options.werfer){
-    var werfer = new WerferClass(config);
-    if(opt.options.on){
-        console.log("Werfer einschalten");
-        werfer.on().then(()=>{
-            console.log("Werfer is on");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    }else{
-        console.log("Werfer ausschalten");
-    }
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+if(opt.options.debug){
+    rl.question("Press any key...",(answ)=>{
+        doJob();
+    });
+}else{
+    doJob();
 }
 
-if(opt.options.sprinkler){
-    var sprinkler = new SprinklerClass(config);    
-    if(opt.options.on){
-        console.log("Sprinkler einschalten");
-        sprinkler.on().then(()=>{
-            console.log("Sprinkler is on");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    }else{
-        console.log("Sprinkler ausschalten");
+function doJob() {
+    if (opt.options.werfer) {
+        var werfer = new WerferClass(config);
+        if (opt.options.on) {
+            console.log("Werfer einschalten");
+            werfer.on().then(() => {
+                console.log("Werfer is on");
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            console.log("Werfer ausschalten");
+        }
     }
-}
 
-if(opt.options.tropfer){
-    var tropfer = new TropferClass(config);
-    if(opt.options.on){
-        console.log("Tropfer einschalten");
-        tropfer.on().then(()=>{
-            console.log("Tropfer is on");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    }else{
-        console.log("Tropfer ausschalten");
+    if (opt.options.sprinkler) {
+        var sprinkler = new SprinklerClass(config);
+        if (opt.options.on) {
+            console.log("Sprinkler einschalten");
+            sprinkler.on().then(() => {
+                console.log("Sprinkler is on");
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            console.log("Sprinkler ausschalten");
+        }
     }
-}
 
-if(opt.options.lamp){
-    var lamp = new LampClass(config);
-    if(opt.options.on){
-        console.log("Lampe einschalten");
-        lamp.on().then(()=>{
-            console.log("Lampe ist ein");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    }else{
-        console.log("Lampe ausschalten");
-        lamp.off().then(()=>{
-            console.log("Lampe ist aus");
-        }).catch((err)=>{
-            console.log(err);
-        });
+    if (opt.options.tropfer) {
+        var tropfer = new TropferClass(config);
+        if (opt.options.on) {
+            console.log("Tropfer einschalten");
+            tropfer.on().then(() => {
+                console.log("Tropfer is on");
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            console.log("Tropfer ausschalten");
+        }
     }
-}
 
-if(opt.options.power){
-    var power = powerM.getInstance(config);
-    if(opt.options.on){
-        console.log("Trafo einschalten");
-        power.on().then(()=>{
-            console.log("Trafo ist ein");
-        }).catch((err)=>{
-            console.log(err);
-        });
-    }else{
-        console.log("Trafo ausschalten");
-        power.off().then(()=>{
-            console.log("Trafo ist aus");
-        }).catch((err)=>{
-            console.log(err);
-        });
+    if (opt.options.lamp) {
+        var lamp = new LampClass(config);
+        if (opt.options.on) {
+            console.log("Lampe einschalten");
+            lamp.on().then(() => {
+                console.log("Lampe ist ein");
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            console.log("Lampe ausschalten");
+            lamp.off().then(() => {
+                console.log("Lampe ist aus");
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }
+
+    if (opt.options.power) {
+        var power = powerM.getInstance(config);
+        if (opt.options.on) {
+            console.log("Trafo einschalten");
+            power.on().then(() => {
+                console.log("Trafo ist ein");
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            console.log("Trafo ausschalten");
+            power.off().then(() => {
+                console.log("Trafo ist aus");
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
     }
 }
